02e7e137f601cda523340291bc71b6e42ca4a304
juhi123 <juhi.saxena@rtcamp.com>
2025-01-16 22:55:05 +0530

    Add cleanup in afterEach

--- tests/e2e/specs/settings/settings.test.js
+++ tests/e2e/specs/settings/settings.test.js
@@ -1,12 +1,22 @@
 const { test, expect } = require('@wordpress/e2e-test-utils-playwright');
 
 test.describe('General Settings', () => {
-    test('changes timezone', async ({ admin, page }) => {
+    let defaultTimezone;
+
+    test.beforeEach(async ({ admin, page }) => {
         await admin.visitAdminPage('options-general.php');
 
         // Capture the default timezone
-        const defaultTimezone = await page.locator('select#timezone_string').inputValue();
+        defaultTimezone = await page.locator('select#timezone_string').inputValue();
+    });
 
+    test.afterEach(async ({ page }) => {
+         // Revert to default timezone 
+         await page.locator('select#timezone_string').selectOption(defaultTimezone);
+         await page.click('#submit');
+    });
+    test('changes timezone', async ({ admin, page }) => {
+      
         // Change timezone
         await page.locator('select#timezone_string').selectOption('Africa/Libreville');
         await page.click('#submit');
@@ -18,11 +28,7 @@ test.describe('General Settings', () => {
 
         // Verify selected timezone persists
         const selectedTimezone = await page.locator('select#timezone_string').inputValue();
-        await expect(selectedTimezone).toBe('Africa/Libreville');
-
+        await expect(selectedTimezone).toBe('Africa/Libreville');       
        
-        // Revert to default timezone (cleanup step)
-        await page.locator('select#timezone_string').selectOption(defaultTimezone);
-        await page.click('#submit');
     });
 });
